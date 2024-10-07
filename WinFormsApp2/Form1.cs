b23c43b5fb1624b043666d90a306aedb9b909270
MarijaGorbunova0 <153904965+MarijaGorbunova0@users.noreply.github.com>
2024-10-07 15:36:08 +0300

    Update Form1.cs

--- WinFormsApp2/Form1.cs
+++ WinFormsApp2/Form1.cs
@@ -1,11 +1,15 @@
+using Microsoft.VisualBasic;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
 using System.DirectoryServices.ActiveDirectory;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Windows.Forms.VisualStyles;
+using System.Xml.Linq;
 
 namespace WinFormsApp2
 {
@@ -18,6 +22,10 @@ namespace WinFormsApp2
         PictureBox pBox;
         CheckBox chk1, chk2;
         private int pictureIndex = 0;
+        ListBox lb;
+        DataGridView dg;
+        DataSet ds;
+        TextBox txt;
         public StartVorm()
         {
             this.Height = 500;
@@ -31,8 +39,10 @@ namespace WinFormsApp2
             tn.Nodes.Add(new TreeNode("Silt"));
             tn.Nodes.Add(new TreeNode("Pilt"));
             tn.Nodes.Add(new TreeNode("Markiruut"));
-            
-            
+            tn.Nodes.Add(new TreeNode("tabel"));
+            tn.Nodes.Add(new TreeNode("Loetelu"));
+            tn.Nodes.Add(new TreeNode("Dialoogi"));
+
             //tn.Nodes.Add(new TreeNode(Silt));
 
             this.Controls.Add(tree);
@@ -115,6 +125,40 @@ namespace WinFormsApp2
                 pBox.BorderStyle = BorderStyle.None;
             }
         }
+        private void Lb_SelectedIndexChanged(object? sender, EventArgs e) {
+            switch (lb.SelectedIndex) {
+                case 0: tree.BackColor = Color.Red; break;
+                case 1: tree.BackColor = Color.Green; break;
+                case 2: tree.BackColor = Color.Blue; break;
+
+            }   
+        }
+        //private void Txt_textChanged(object? sender, EventArgs e) {
+        //    lbl.Text = txt.Text;
+        //}
+        private void Lb_changeColor(object? sender, EventArgs e)
+        {
+            //if (lb.CheckedItems )
+        }
+        private void Dg_RowHeaderMouseClick(object? sender,DataGridViewCellMouseEventArgs e )
+        {
+            lbl.Text = dg.Rows[e.RowIndex].Cells[0].Value.ToString() + "hind" + dg.Rows[e.RowIndex].Cells[1].Value.ToString();
+
+        }
+        private void AddItemsXml(object? sender, EventArgs e)
+        {
+            DataRow newROW = ds.Tables["food"].NewRow();
+            newROW["name"] = Interaction.InputBox("name");
+            newROW["price"] = Interaction.InputBox("price");
+            newROW["description"] = Interaction.InputBox("description");
+            newROW["calories"] = Interaction.InputBox("calories");
+            ds.Tables["food"].Rows.Add(newROW);
+            ds.WriteXml(@"..\..\..\menu.xml");
+            MessageBox.Show("yayyy");
+
+
+
+        }
         private void Tree_AfterSelect(object? sender, TreeViewEventArgs e)
         {
             if (e.Node.Text == "Nupp")
@@ -129,11 +173,42 @@ namespace WinFormsApp2
             {
                 Controls.Add(pBox);
             }
-            else if ( e.Node.Text == "Markiruut")
+            else if (e.Node.Text == "Markiruut")
             {
                 Controls.Add(chk1);
                 Controls.Add(chk2);
             }
-        }
+            else if (e.Node.Text == "Loetelu")
+            {
+                lb = new ListBox();
+                lb.Items.Add("uks");
+                lb.Items.Add("kaks");
+                lb.Items.Add("kolm");
+                lb.Location = new Point(300, 300);
+                Controls.Add(lb);
+            }
+            else if (e.Node.Text == "tabel")
+            {
+                ds = new DataSet("XML fail");
+                ds.ReadXml(@"..\..\..\menu.xml");
+                dg = new DataGridView();
+                dg.Location = new Point(150, lbl.Height + btn.Height + pBox.Height + lb.Height);
+                dg.DataSource = ds;
+                dg.DataMember = "food";
+                //dg.RowHeaderMouseClick += Dg_RowHeaderMouseClick();
+                Controls.Add(dg);
+
+            }
+            else if (e.Node.Text == "Dialoogi")
+            {
+                
+                MessageBox.Show("Dialoog", "see on lihtne aken");
+                var vastus = MessageBox.Show("Sisesatme andmes", "kas tahad input button kustutada?", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (vastus == DialogResult.Yes)
+                {
+                  
+                }   
+            }
+        }  
     }
 }
